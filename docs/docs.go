e49cb8f1ade47b107e7106db9b9128e087b533ab
Jobayer Ahmed <Jobayer6735@gmail.com>
2024-05-23 11:19:32 +0600

    Modified the swagger host name

--- docs/docs.go
+++ docs/docs.go
@@ -72,7 +72,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "grotesque-vivianne-splendid-ab71dd99.koyeb.app",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Kubernetes API",
